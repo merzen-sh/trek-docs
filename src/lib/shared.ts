2da1a94399eb318b1b4e976649a51d451f88e34b
merzen-sh <yuthana.wareesri.2001@gmail.com>
2026-07-18 11:02:49 +0700

    initial commit

---
+++ src/lib/shared.ts
@@ -0,0 +1,9 @@
+export const appName = "TREK";
+export const docsRoute = "/docs";
+export const docsImageRoute = "/og/docs";
+
+export const gitConfig = {
+  user: "trek-scripts",
+  repo: "trek-website",
+  branch: "main",
+};
